ea4ea97ea091a8c4d542326f39c61d3f78e83076
baige6// <zanfxb@gmail.com.com//>
2019-09-18 10:49:41 +0800

    Site updated: 2019-09-18 10:49:40

--- js/src/love-click.js
+++ js/src/love-click.js
@@ -1,55 +1,51 @@
-jQuery(document).ready(function($) { 
-    $("html,body").click(function(e){ 
-        var dfs=[
-            "���ղŵĵ��ʹ�����ַſ���"+Math.floor(Math.random()*100)+"̥���ߡ�",
-            "���ղŵĵ��ʹ��ʯ������ɽ������",
-            "���ղŵĵ��ʹ�޼��ֻ���һ��Ů����",
-            "���ղŵĵ��ʹС�׹�˾�չ��˸�ͨ��˾",
-            "���ղŵĵ��������"+Math.random()*10000+"�������һ�ź���",
-            "���ղŵĵ��ʹͷ������ں�Ũ����",
-            "���ղŵĵ��ʹ���Ĳ�����̮����", 
-            "���ղŵĵ��ʹ�������п�����ˢ��", 
-            "���ղŵĵ��ʹ���ʿ˳���������ʮ��", 
-            "���ղŵĵ��Ϩ������ɫ�ռ�Ŷ���������", 
-            "���ղŵĵ��������һ��С����", 
-            "���ղŵĵ��ʹ�����ɿ���תΪ�պ�", 
-            "���ղŵĵ�����������͹�һ����һ����",
-            "���ղŵĵ��ʹ�����"+Math.floor(Math.random()*100)+"��������", 
-            "���ղŵĵ��ʹ��"+Math.floor(Math.random()*10000)+"�����������", 
-            "���ղŵĵ����̫��������һ�Ź���", 
-            "���ղŵĵ��������������ײ����������",
-            "���ղŵĵ����������������"+(new Date()).getFullYear()+"��"+((new Date()).getMonth()+1)+"��"+(new Date()).getUTCDate()+"��23:59:59����",
-            "���ղŵĵ������������ϵͳ",
-            "���ղŵĵ��ʹл�ټƻ�����������",
-            "���ղŵĵ��ʹ���İ��������忪��",
-            "���ղŵĵ��������"+Math.random()*10000+"������һ�����ǵı�ը",
-            "���ղŵĵ��ʹ�����в�����һЩ������",
-            "���ղŵĵ�����������������ϵĵ���",
-            "���ղŵĵ��ʹһ̨ŵ����ˤ����",
-            "���ղŵĵ��ʹ360���̵�����",
-            "���ղŵĵ��С�׷�����MIX�����ֻ�",
-            "ǧ��Ҫ�ٴε�����",
-            "���ǧ��Ҫ�ٴε�����"
-        ];
-        var n=Math.floor(Math.random() * dfs.length + 1)-1;   //�����ȡһ������
-        var $i=$("<p/>").text(dfs[n]);      //�½�һ��b��ǩ������ʾ����Ļ���
-        var x=e.pageX,y=e.pageY;            //��ȡ�������x����y
-        $i.css({                            //Ϊ��ǩ����cssֵ
-            "z-index":99999, 
-            "top":y-20, 
-            "left":x, 
-            "position":"absolute", 
-            "color":"#E94F06",
-            "font-family":"΢���ź�",
-            "cursor":"default",
-            "-moz-user-select": "none",
-            "-webkit-user-select": "none",
-            "-ms-user-select": "none",
-            "-khtml-user-select": "none",
-            "user-select": "none",
-        }); 
-        $("body").append($i);               //��β������
-        $i.animate( {"top":y-180,"opacity":0}, 1500, function(){$i.remove();});     //��������
-        e.stopPropagation();
-    });
-}); 
+/* ������Ч */
+
+!function (e, t, a) {
+  function r() {
+    for (var e = 0; e < s.length; e++) s[e].alpha <= 0 ? (t.body.removeChild(s[e].el), s.splice(e, 1)) : (s[e].y--, s[e].scale += .004, s[e].alpha -= .013, s[e].el.style.cssText = "left:" + s[e].x + "px;top:" + s[e].y + "px;opacity:" + s[e].alpha + ";transform:scale(" + s[e].scale + "," + s[e].scale + ") rotate(45deg);background:" + s[e].color + ";z-index:99999");
+    requestAnimationFrame(r)
+  }
+
+  function n() {
+    var t = "function" == typeof e.onclick && e.onclick;
+    e.onclick = function (e) {
+      t && t(), o(e)
+    }
+  }
+
+  function o(e) {
+    var a = t.createElement("div");
+    a.className = "heart", s.push({
+      el: a,
+      x: e.clientX - 5,
+      y: e.clientY - 5,
+      scale: 1,
+      alpha: 1,
+      color: c()
+    }), t.body.appendChild(a)
+  }
+
+  function i(e) {
+    var a = t.createElement("style");
+    a.type = "text/css";
+    try {
+      a.appendChild(t.createTextNode(e))
+    } catch (t) {
+      a.styleSheet.cssText = e
+    }
+    t.getElementsByTagName("head")[0].appendChild(a)
+  }
+
+  function c() {
+    return "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + ")"
+  }
+  var s = [];
+  e.requestAnimationFrame = e.requestAnimationFrame || e.webkitRequestAnimationFrame || e.mozRequestAnimationFrame || e.oRequestAnimationFrame || e.msRequestAnimationFrame || function (e) {
+    setTimeout(e, 1e3 / 60)
+  }, i(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -5px;}.heart:before{left: -5px;}"), n(), r()
+}(window, document);
+
+
+function delay() {
+    $(".buryit").removeAttr("onclick");
+}
